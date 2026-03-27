017369b9cf7b9fa45811b009c1aa9ca9e561d263
toufikbazem <toufikbazem@gmail.com>
2026-03-27 16:24:22 +0100

    chore: setup PostgreSQL connection with Supabase

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import db from "./src/config/db.js";
 
 const app = express();
 dotenv.config();
@@ -14,6 +15,18 @@ app.listen(5000, () => {
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.get("/test", async (req, res) => {
+  try {
+    const user = await db.query("SELECT * FROM users WHERE email = $1", [
+      "toufikbazem@gmail.com",
+    ]);
+    console.log(user);
+    res.json({ msg: "test" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
