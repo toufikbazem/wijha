95d0fc5e136cd9ecd1d7e13d5b975afc66f082a5
toufikbazem <toufikbazem@gmail.com>
2026-03-31 10:55:25 +0200

    feat: add requireAuth middleware

---
+++ server/src/middleware/auth.middleware.js
@@ -0,0 +1,22 @@
+// middleware/auth.middleware.js
+import jwt from "jsonwebtoken";
+
+export const requireAuth = (roles = []) => {
+  return (req, res, next) => {
+    try {
+      const token = req.cookies.token;
+      if (!token) return res.status(401).json({ error: "Unauthorized" });
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+
+      if (roles.length && !roles.includes(decoded.role)) {
+        return res.status(403).json({ error: "Forbidden" });
+      }
+
+      next();
+    } catch (err) {
+      return res.status(401).json({ error: "Invalid token" });
+    }
+  };
+};
