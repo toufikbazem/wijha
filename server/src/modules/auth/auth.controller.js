aaa58f915cd30b86bc3f25c7924f113ad7ce2d13
toufikbazem <toufikbazem@gmail.com>
2026-03-31 10:33:02 +0200

    feat: add forgot password API endpoints

--- server/src/modules/auth/auth.controller.js
+++ server/src/modules/auth/auth.controller.js
@@ -6,8 +6,153 @@ import { sendVerifyEmail } from "../../utils/send_verify_email.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const register = (req, res) => {};
-const login = (req, res) => {};
+const register = async (req, res) => {
+  const {
+    email,
+    password,
+    role,
+    firstName,
+    lastName,
+    address,
+    phoneNumber,
+    companyName,
+    industry,
+    size,
+  } = req.body;
+
+  if (role === "jobseeker") {
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !password ||
+      !address ||
+      !phoneNumber
+    ) {
+      res
+        .status(400)
+        .json({ message: "All fields are required for job seekers." });
+    }
+  } else if (role === "employer") {
+    if (!email || !password || !companyName || !industry || !size || !address) {
+      res
+        .status(400)
+        .json({ message: "All fields are required for employers." });
+    }
+  } else {
+    res.status(400).json({ message: "Invalid user role." });
+  }
+
+  let newUser;
+
+  try {
+    const existingUser = await db.query("SELECT * FROM users WHERE email=$1", [
+      email,
+    ]);
+    if (existingUser.rows.length > 0) {
+      return res.status(409).json({ message: "User already exists." });
+    }
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    newUser = await db.query(
+      "INSERT INTO users (email, password, role) VALUES ($1, $2, $3) RETURNING id",
+      [email, hashedPassword, role],
+    );
+    await db.query("INSERT INTO password_reset_tokens (user_id) VALUES ($1)", [
+      newUser.rows[0].id,
+    ]);
+    await db.query(
+      "INSERT INTO email_verification_tokens (user_id) VALUES ($1)",
+      [newUser.rows[0].id],
+    );
+
+    sendVerifyEmail(email, newUser.rows[0].id);
+
+    if (role === "jobseeker") {
+      await db.query(
+        "INSERT INTO jobseeker (user_id, first_name, last_name, address, phone_number) VALUES ($1, $2, $3, $4, $5)",
+        [newUser.rows[0].id, firstName, lastName, address, phoneNumber],
+      );
+    } else if (role === "employer") {
+      await db.query(
+        "INSERT INTO company (user_id, company_name, industry, size, address) VALUES ($1, $2, $3, $4, $5)",
+        [newUser.rows[0].id, companyName, industry, size, address],
+      );
+    }
+    res.status(201).json({ message: "User registered successfully." });
+  } catch (error) {
+    await db.query("DELETE FROM users WHERE id=$1", [newUser.rows[0].id]);
+    res.status(500).json(error.message);
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password, rememberMe } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Email and password are required." });
+  }
+
+  try {
+    const user = await db.query("SELECT * FROM users WHERE email=$1", [email]);
+    if (user.rows.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "Password or email is incorrect" });
+    }
+    const isPasswordValid = bcryptjs.compareSync(
+      password,
+      user.rows[0].password,
+    );
+    if (!isPasswordValid) {
+      return res
+        .status(401)
+        .json({ message: "Password or email is incorrect." });
+    }
+
+    if (user.rows[0].status === "desactivated") {
+      res
+        .status(403)
+        .json({ message: "Account desactivated, contact support" });
+    }
+
+    if (user.rows[0].role === "jobseeker") {
+      var userData = await db.query(
+        "SELECT * FROM jobseeker WHERE user_id=$1",
+        [user.rows[0].id],
+      );
+    } else {
+      var userData = await db.query("SELECT * FROM company WHERE user_id=$1", [
+        user.rows[0].id,
+      ]);
+    }
+
+    const token = jwt.sign(
+      { userId: user.rows[0].id, role: user.rows[0].role },
+      process.env.JWT_SECRET,
+      { expiresIn: rememberMe ? "7d" : "1d" },
+    );
+
+    const { password: pass, ...rest1 } = user.rows[0];
+
+    res
+      .status(200)
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "Lax",
+        path: "/",
+        maxAge: rememberMe ? 7 * 24 * 60 * 60 * 1000 : 24 * 60 * 60 * 1000,
+      })
+      .json({
+        ...rest1,
+        ...userData.rows[0],
+      });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
 const logout = (req, res) => {};
 
 const forgotPassword = async (req, res) => {
@@ -121,11 +266,8 @@ const sendVerificationEmail = async (req, res) => {
         .status(404)
         .json({ message: "User not exist with this email" });
     }
-    const token = jwt.sign({ id: user.rows[0].id }, process.env.JWT_SECRET, {
-      expiresIn: "15m",
-    });
 
-    sendVerifyEmail(user.rows[0].email, token, user.rows[0].id);
+    sendVerifyEmail(user.rows[0].email, user.rows[0].id);
 
     res.status(200).json({ message: "Verification email sent" });
   } catch (error) {
--- server/src/utils/send_verify_email.js
+++ server/src/utils/send_verify_email.js
@@ -1,9 +1,13 @@
 import db from "../config/db.js";
 import { sendEmail } from "./send-email.js";
+import jwt from "jsonwebtoken";
 
-export const sendVerifyEmail = async (email, token, user_id) => {
-  const url = `http://localhost:3000/verify-email?token=${token}`;
+export const sendVerifyEmail = async (email, user_id) => {
   try {
+    const token = jwt.sign({ id: user_id }, process.env.JWT_SECRET, {
+      expiresIn: "15m",
+    });
+    const url = `http://localhost:3000/verify-email?token=${token}`;
     await db.query(
       "UPDATE email_verification_tokens SET token = $1, expire_at = $2, created_at = $3 WHERE user_id = $4",
       [token, new Date(Date.now() + 24 * 60 * 60 * 1000), new Date(), user_id],
@@ -46,6 +50,7 @@ export const sendVerifyEmail = async (email, token, user_id) => {
     </div>`,
     });
   } catch (error) {
+    console.log(error);
     throw new Error("Failed to send verification email");
   }
 };
