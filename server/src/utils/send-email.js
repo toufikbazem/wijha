529e678ef5c5db3c5c4315a082ae2b797f7b1c85
toufikbazem <92893227+toufikbazem@users.noreply.github.com>
2026-04-30 00:41:55 +0200

    Fix formatting issue in sendEmail function

--- server/src/utils/send-email.js
+++ server/src/utils/send-email.js
@@ -4,7 +4,7 @@ export const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
 port: 587,
-secure: false
+secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
