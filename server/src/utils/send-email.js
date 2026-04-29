8b42581d8f5d0a377b785f6f896f76bf1a33387c
toufikbazem <92893227+toufikbazem@users.noreply.github.com>
2026-04-30 00:40:13 +0200

    Change email transport settings to use port 587

--- server/src/utils/send-email.js
+++ server/src/utils/send-email.js
@@ -3,8 +3,8 @@ import nodemailer from "nodemailer";
 export const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+port: 587,
+secure: false
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
