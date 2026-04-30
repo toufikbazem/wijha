994a12036c9a9e0f50bd7b81fd49b6cff4b4c0e1
toufikbazem <toufikbazem@gmail.com>
2026-04-30 11:54:17 +0200

    project edits

--- server/src/utils/send-email.js
+++ server/src/utils/send-email.js
@@ -1,20 +1,38 @@
 import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
+const resend = new Resend("re_MeZJPGZX_B9DuyPBj45ZUFoeGQBNmJcci");
+
+// export const sendEmail = async ({ to, subject, html }) => {
+//   const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 587,
+//     secure: false,
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASSWORD,
+//     },
+//   });
+
+//   return transporter.sendMail({
+//     from: `toufikbazem@gmail.com`,
+//     to,
+//     subject,
+//     html,
+//   });
+// };
 export const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  try {
+    const response = await resend.emails.send({
+      from: "Wijha <noreply@wijha-dz.com>",
+      to,
+      subject,
+      html,
+    });
 
-  return transporter.sendMail({
-    from: `toufikbazem@gmail.com`,
-    to,
-    subject,
-    html,
-  });
+    return response;
+  } catch (error) {
+    console.error("Email error:", error);
+    throw error;
+  }
 };
