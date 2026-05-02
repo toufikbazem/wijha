48c0fc88fad822b294b4d40397be15c1d5537684
toufikbazem <92893227+toufikbazem@users.noreply.github.com>
2026-05-02 15:25:28 +0200

    Update auth.controller.js

--- server/src/modules/auth/auth.controller.js
+++ server/src/modules/auth/auth.controller.js
@@ -205,7 +205,6 @@ const login = async (req, res) => {
         httpOnly: true,
         secure: true,
         sameSite: "none",
-        domain: "https://wijha-z3rf.onrender.com",
         path: "/",
         maxAge: rememberMe ? 7 * 24 * 60 * 60 * 1000 : 24 * 60 * 60 * 1000,
       })
